7c51a4c758dd21a252543b3366c0159c640cc5ab
Alejandro Gonzalez <alejandro.gonzalez@groove-x.com>
2019-05-19 23:18:07 +0900

    Added setup.py and installation of samplepkg in readthedocs

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+version = '0.1.0'
+
+setup(name='samplepkg',
+      version=version,
+      description='A sample package used to test docs build with Sphinx and readthedocs.',
+      packages=['samplepkg'])
